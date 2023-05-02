d86d2365104eb3674cd1c56f492b1cfc132da3d9
KsenyaKoss <roxenn34@gmail.com>
2023-05-02 14:04:02 +0200

    2.05.2023 practice

---
+++ src/pages/UsersPage/UsersPage.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchUsers } from 'redux/Users/usersOperations';
+import { selectUsers } from 'redux/Users/usersSelectors';
+
+export const UsersPage = () => {
+  const users = useSelector(selectUsers);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  console.log(users);
+  return (
+ <>
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>
+          <Link to={user.id}>{user.name}</Link>
+        </li>
+      ))}
+    </ul>
+    <Link to='add'>Add User</Link></>
+  );
+};
---
+++ src/redux/Users/usersSlice.jsx
@@ -0,0 +1,96 @@
+const { createSlice } = require('@reduxjs/toolkit');
+const { fetchUsers, fetchUser, deleteUser, addUser } = require('./usersOperations');
+
+const initialState = {
+  users: [],
+  isLoading: false,
+  error: null,
+  currentUser: null,
+};
+
+// const usersSlice = createSlice({
+//   name: 'users',
+//   initialState,
+//   extraReducers: {
+//     [fetchUsers.pending](state) {
+//       state.isLoading = true;
+//     },
+//     [fetchUsers.fulfilled](state, action) {
+//       state.isLoading = false;
+//       state.error = null;
+//       state.users = action.payload;
+//     },
+//     [fetchUsers.rejected](state, action) {
+//       state.isLoading = false;
+//       state.error = action.payload;
+//     },
+//     [fetchUser.pending](state) {
+//       state.isLoading = true;
+//     },
+//     [fetchUser.fulfilled](state, action) {
+//       state.isLoading = false;
+//       state.error = null;
+//       state.currentUser = action.payload;
+//     },
+//     [fetchUser.rejected](state, action) {
+//       state.isLoading = false;
+//       state.error = action.payload;
+//     },
+//     [deleteUser.pending](state) {
+//       state.isLoading = true;
+//     },
+//     [deleteUser.fulfilled](state, action) {
+//       state.isLoading = false;
+//       state.error = null;
+//       state.users = state.users.filter(user => user.id !== action.payload);
+//     },
+//     [deleteUser.rejected](state, action) {
+//       state.isLoading = false;
+//       state.error = action.payload;
+//     },
+//   },
+// });
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  extraReducers: builder => {
+    builder
+
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.users = action.payload;
+      })
+      .addCase(fetchUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.currentUser = action.payload;
+      })
+      .addCase(deleteUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.users = state.users.filter(user => user.id !== action.payload);
+      })
+      .addCase(addUser.fulfilled, (state, {payload}) => {
+        state.users.push(payload);
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addMatcher(
+        action => action.type.endsWith('pending'),
+        state => {
+          state.isLoading = true;
+        }
+      )
+      .addMatcher(
+        action => action.type.endsWith('rejected'),
+        (state, action) => {
+          state.isLoading = false;
+          state.error = action.payload;
+        }
+      );
+  },
+});
+
+export const usersReducer = usersSlice.reducer;
